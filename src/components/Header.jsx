88aba9886c15bd47016f14554122ae7fde8bc810
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-02-18 08:56:07 +0200

    Main Social Card Created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,49 @@
+import facebookIcon from "../assets/icon-facebook.svg";
+import twitterIcon from "../assets/icon-twitter.svg";
+import instagramIcon from "../assets/icon-instagram.svg";
+import youtubeIcon from "../assets/icon-youtube.svg";
+import upIcon from "../assets/icon-up.svg";
+import downIcon from "../assets/icon-down.svg";
+import { useState } from "react";
+
 import SocialCardBox from "./SocialCardBox";
 
 const Header = () => {
+  const [cardData, setCardData] = useState([
+    {
+      icon: facebookIcon,
+      userName: "@Hema",
+      followers: 1987,
+      todayNum: 12,
+      state: upIcon,
+    },
+    {
+      icon: twitterIcon,
+      userName: "@Ali",
+      followers: 1044,
+      todayNum: 99,
+      state: upIcon,
+    },
+    {
+      icon: instagramIcon,
+      userName: "@Ahmed",
+      followers: "11K",
+      todayNum: 1099,
+      state: upIcon,
+    },
+    {
+      icon: youtubeIcon,
+      userName: "@Hassan",
+      subscribers: 8239,
+      todayNum: 144,
+      state: downIcon,
+    },
+  ]);
   return (
-    <section className="container bg-[#ddd] p-5">
-      <div className="1 flex      flex-col md:flex-row  md:justify-between md:items-center">
+    <section className="container bg-[#ddd] p-10">
+      <div className="1 flex flex-col md:flex-row  md:justify-between md:items-center font-bold">
         <div className="1.1 flex  flex-col  md:flex-col">
-          <h1>Social Media Dashboard</h1>
+          <h1 className=" text-3xl">Social Media Dashboard</h1>
           <h3>Total Followers: 23.004</h3>
         </div>
         <div className="1.2 flex justify-between md:items-center">
@@ -13,8 +51,18 @@ const Header = () => {
           <input type="checkbox" />
         </div>
       </div>
-      <div>
-        <SocialCardBox />
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-5  mt-5">
+        {cardData.map((cardData) => (
+          <SocialCardBox
+            key={cardData.userName}
+            name={cardData.userName}
+            icon={cardData.icon}
+            followers={cardData.followers}
+            subscribers={cardData.subscribers}
+            state={cardData.state}
+            todayNum={cardData.todayNum}
+          />
+        ))}
       </div>
     </section>
   );
--- src/components/SocialCardBox.jsx
+++ src/components/SocialCardBox.jsx
@@ -1,54 +1,25 @@
-import facebookIcon from "../assets/icon-facebook.svg";
-import twitterIcon from "../assets/icon-twitter.svg";
-import instagramIcon from "../assets/icon-instagram.svg";
-import youtubeIcon from "../assets/icon-youtube.svg";
-import upIcon from "../assets/icon-up.svg";
-import downIcon from "../assets/icon-down.svg";
-import { useState } from "react";
-
-const SocialCardBox = () => {
-  const [cardBox, setCardBox] = useState([
-    {
-      icon: facebookIcon,
-      userName: "@Hema",
-      followers: 1987,
-      todayNum: 5,
-      state: upIcon,
-    },
-    {
-      icon: twitterIcon,
-      userName: "@Ali",
-      followers: 1044,
-      todayNum: 5,
-      state: upIcon,
-    },
-    {
-      icon: instagramIcon,
-      userName: "@Ahmed",
-      followers: "11K",
-      todayNum: 5,
-      state: upIcon,
-    },
-    {
-      icon: youtubeIcon,
-      userName: "@Hassan",
-      subscribers: 8239,
-      todayNum: 5,
-      state: downIcon,
-    },
-  ]);
+const SocialCardBox = ({
+  name,
+  icon,
+  followers,
+  state,
+  todayNum,
+  subscribers,
+}) => {
   return (
-    <div className="container max-w-full max-h-full w-[250px] h-[250px] rounded-md bg-green-500">
-      <div className=" flex-col element-centered gap-10 p-5">
-        <div className="flex">
-          <img src={facebookIcon} alt="img" />
-          <div className="bg-red-500"> hema</div>
+    <div className="container max-w-full max-h-full w-[300px] h-[275px] rounded-md border-solid border-t-4  border-[red]">
+      <div className=" flex-col element-centered gap-5 p-10">
+        <div className="flex gap-3">
+          <img src={icon} alt="img" className="object-contain w-6 h-6" />
+          <div className="">{name}</div>
         </div>
-        <h1>9000</h1>
-        <p>followers</p>
-        <div className="flex">
-          <img src={upIcon} alt="img" />
-          <div className="bg-red-500">345 Today</div>
+        <h1 className="font-bold text-6xl">
+          {followers ? followers : subscribers}
+        </h1>
+        <p className="uppercase text-sm tracking-[5px]">followers</p>
+        <div className="flex gap-2 items-center">
+          <img src={state} alt="img" className="object-contain w-5 h-5" />
+          <div className=""> {todayNum} Today</div>
         </div>
       </div>
     </div>
